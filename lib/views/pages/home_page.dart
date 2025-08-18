90084845a82d55779f6fca72bc54274493c86eb9
Natalia Kals <nataliakals03@gmail.com>
2025-08-19 11:59:13 +1200

    Formating page with sigle child

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -9,32 +9,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          HeroWidget(title: 'Home'),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: Card(
-              child: Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Welcome to the Home Page!',
-                      style: KTextStyles.titleTealText,
-                    ),
-                    Text(
-                      'This is a simple example of using a Hero widget in Flutter.',
-                      style: KTextStyles.descriptionText,
-                    ),
-                  ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeroWidget(title: 'Home'),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              child: Card(
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Welcome to the Home Page!',
+                        style: KTextStyles.titleTealText,
+                      ),
+                      Text(
+                        'This is a simple example of using a Hero widget in Flutter.',
+                        style: KTextStyles.descriptionText,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -3,15 +3,19 @@ import 'package:hello_flutter/views/widget_tree.dart';
 import 'package:hello_flutter/widgets/hero_widget.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController controllerEmail = TextEditingController();
-  TextEditingController controllerPassword = TextEditingController();
+  TextEditingController controllerEmail = TextEditingController(text: '123');
+  TextEditingController controllerPassword = TextEditingController(text: '123');
+  String conformedEmail = '123';
+  String conformedPassword = '123';
 
   @override
   void dispose() {
@@ -25,53 +29,66 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login Page')),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            HeroWidget(title: 'Login'),
-            SizedBox(height: 20),
-            TextField(
-              controller: controllerEmail,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-              onEditingComplete: () => setState(() {}),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: controllerPassword,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-              onEditingComplete: () => setState(() {}),
-            ),
-            SizedBox(height: 20),
-            FilledButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return WidgetTree();
-                    },
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                HeroWidget(title: widget.title),
+                SizedBox(height: 20),
+                TextField(
+                  controller: controllerEmail,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
-                );
-              },
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 50),
-              ),
-              child: Text('Login'),
+                  onEditingComplete: () => setState(() {}),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: controllerPassword,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  onEditingComplete: () => setState(() {}),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    onLoginButtonPressed();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 50),
+                  ),
+                  child: Text(widget.title),
+                ),
+                SizedBox(height: 50),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  void onLoginButtonPressed() {
+    if (conformedEmail == controllerEmail.text &&
+        conformedPassword == controllerEmail.text) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return WidgetTree();
+          },
+        ),
+      );
+    }
   }
 }
