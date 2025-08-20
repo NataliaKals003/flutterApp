5c5f2ea7d99f4e0cc54f4b839a6ba8919bb2c60a
Natalia Kals <nataliakals03@gmail.com>
2025-08-21 11:19:54 +1200

    Adding an api url and setting to the field in course page

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,18 +1,66 @@
-import 'package:flutter/material.dart';
-import 'package:flutterApp/widgets/hero_widget.dart';
+import 'dart:convert';
 
-class CoursePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutterApp/data/classes/activity_class.dart';
+import 'package:flutterApp/widgets/hero_widget.dart';
+import 'package:http/http.dart' as http;
+
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+    // Additional initialization if needed
+  }
+
+  Future getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return Activity.fromJson(
+        jsonDecode(response.body) as Map<String, dynamic>,
+      );
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: SingleChildScrollView(
-          child: Column(children: [HeroWidget(title: 'Course')]),
-        ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            Activity activity = snapshot.data;
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    HeroWidget(title: activity.activity),
+                    Text(activity.activity),
+                  ],
+                ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return Center(child: Text('No data available.'));
+          }
+        },
       ),
     );
   }
